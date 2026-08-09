9b3e5ee99021163aed12b63c11087fd24ab3340b
Andrew Gowans <andrewgowans23@googlemail.com>
2026-08-09 20:26:44 +0100

    Refresh Record Hunter viewer 2026-08-09

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='record-hunter-2026-08-09T18-36-26';
+const CACHE='record-hunter-2026-08-09T19-26-40';
 const SHELL=['./','./index.html','./data/record-hunter.json','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
